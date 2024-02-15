861d5d332ac8768385977d72357848dc991eabf1
Czyz7 <czyzhernandez@outlook.com>
2024-02-15 14:54:16 -0800

    Initial Commit

---
+++ Guitar Tuner/Guitar_TunerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guitar_TunerApp.swift
+//  Guitar Tuner
+//
+//  Created by Czyz Hernandez on 2/15/24.
+//
+
+import SwiftUI
+
+@main
+struct Guitar_TunerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
